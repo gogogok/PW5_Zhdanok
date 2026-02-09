457cbcf9f237c57ea46e1cca9e0121571e0dec9e
Daria Zhdanok <knemune@gmail.com>
2026-02-09 09:52:08 +0300

    Initial Commit

---
+++ PW5_Zhdanok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PW5_Zhdanok
+//
+//  Created by Дарья Жданок on 9.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
